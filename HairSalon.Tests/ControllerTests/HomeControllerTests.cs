5545fc523bfa1b344fd269e94bbcd7d5cbcb90db
stuart gill <stuart.a.gill@gmail.com>
2018-12-07 09:39:12 -0800

    build and initialize project structure and files

---
+++ HairSalon.Tests/ControllerTests/HomeControllerTests.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using HairSalon.Controllers;
+using HairSalon.Models;
+
+namespace HairSalon.Tests
+{
+    [TestClass]
+    public class HomeControllerTest
+    {
+    }
+}
---
+++ HairSalon/Controllers/HomeController.cs
@@ -0,0 +1,10 @@
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using HairSalon.Models;
+
+namespace HairSalon.Controllers
+{
+    public class HomeController : Controller
+    {
+    }
+}
