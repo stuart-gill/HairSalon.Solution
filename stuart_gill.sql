526f3681d0f46cb6829b0f848926c2cda34ca625
Kaveh <lcaveh@gmail.com>
2018-12-18 22:00:05 -0800

    adds updated databases

--- stuart_gill.sql
+++ stuart_gill.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:8889
--- Generation Time: Dec 18, 2018 at 06:08 AM
+-- Generation Time: Dec 19, 2018 at 06:58 AM
 -- Server version: 5.6.35
 -- PHP Version: 7.0.15
 
@@ -49,8 +49,8 @@ CREATE TABLE `specialties` (
 --
 
 INSERT INTO `specialties` (`id`, `name`) VALUES
-(1, 'curls'),
-(2, 'mom bobs');
+(5, 'buzz cuts'),
+(6, 'long hair');
 
 -- --------------------------------------------------------
 
@@ -60,17 +60,8 @@ INSERT INTO `specialties` (`id`, `name`) VALUES
 
 CREATE TABLE `stylists` (
   `stylist_id` int(11) NOT NULL,
-  `stylist_name` varchar(256) NOT NULL,
-  `specialty` varchar(256) NOT NULL
+  `stylist_name` varchar(256) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
--- Dumping data for table `stylists`
---
-
-INSERT INTO `stylists` (`stylist_id`, `stylist_name`, `specialty`) VALUES
-(3, 'Rikki', ''),
-(4, 'Tikki', '');
 
 -- --------------------------------------------------------
 
@@ -83,6 +74,34 @@ CREATE TABLE `stylists_specialties` (
   `stylist_id` int(11) NOT NULL,
   `specialty_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `stylists_specialties`
+--
+
+INSERT INTO `stylists_specialties` (`id`, `stylist_id`, `specialty_id`) VALUES
+(54, 3, 0),
+(55, 3, 0),
+(56, 3, 0),
+(57, 3, 0),
+(58, 0, 0),
+(59, 0, 0),
+(60, 0, 0),
+(61, 4, 0),
+(62, 4, 0),
+(63, 4, 0),
+(64, 4, 0),
+(65, 3, 0),
+(66, 3, 0),
+(67, 0, 2),
+(68, 0, 2),
+(69, 3, 2),
+(70, 4, 2),
+(71, 5, 2),
+(72, 4, 1),
+(73, 4, 3),
+(74, 0, 6),
+(75, 6, 5);
 
 --
 -- Indexes for dumped tables
@@ -120,22 +139,22 @@ ALTER TABLE `stylists_specialties`
 -- AUTO_INCREMENT for table `clients`
 --
 ALTER TABLE `clients`
-  MODIFY `client_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+  MODIFY `client_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
 --
 -- AUTO_INCREMENT for table `specialties`
 --
 ALTER TABLE `specialties`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 --
 -- AUTO_INCREMENT for table `stylists`
 --
 ALTER TABLE `stylists`
-  MODIFY `stylist_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `stylist_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 --
 -- AUTO_INCREMENT for table `stylists_specialties`
 --
 ALTER TABLE `stylists_specialties`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=76;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
--- stuart_gill_test.sql
+++ stuart_gill_test.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:8889
--- Generation Time: Dec 18, 2018 at 06:09 AM
+-- Generation Time: Dec 19, 2018 at 06:59 AM
 -- Server version: 5.6.35
 -- PHP Version: 7.0.15
 
@@ -69,6 +69,22 @@ CREATE TABLE `stylists_specialties` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
+-- Dumping data for table `stylists_specialties`
+--
+
+INSERT INTO `stylists_specialties` (`id`, `stylist_id`, `specialty_id`) VALUES
+(19, 0, 0),
+(20, 0, 0),
+(21, 0, 0),
+(22, 85, 66),
+(23, 94, 72),
+(24, 96, 73),
+(25, 104, 79),
+(26, 106, 80),
+(27, 114, 81),
+(28, 115, 83);
+
+--
 -- Indexes for dumped tables
 --
 
@@ -104,22 +120,22 @@ ALTER TABLE `stylists_specialties`
 -- AUTO_INCREMENT for table `clients`
 --
 ALTER TABLE `clients`
-  MODIFY `client_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+  MODIFY `client_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=136;
 --
 -- AUTO_INCREMENT for table `specialties`
 --
 ALTER TABLE `specialties`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=84;
 --
 -- AUTO_INCREMENT for table `stylists`
 --
 ALTER TABLE `stylists`
-  MODIFY `stylist_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+  MODIFY `stylist_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=116;
 --
 -- AUTO_INCREMENT for table `stylists_specialties`
 --
 ALTER TABLE `stylists_specialties`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=29;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
